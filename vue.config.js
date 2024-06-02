e40c7f6ee4e8f3618c263fb330b975daae31ebd4
huyanzhe <2061217163@qq.com>
2024-06-02 18:39:12 +0800

    hyz

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/user': {
+        target: 'http://localhost:8000', // Django服务器的地址
+        changeOrigin: true,
+
+      },
+    },
+  },
 })
 
