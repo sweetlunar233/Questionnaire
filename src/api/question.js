6cf0e8283e7cefacaeb84add62a67aabcedafd83
TieZhuGieGie <dcx1378832571@163.com>
2024-06-08 23:36:16 +0800

    TieZhu

--- src/api/question.js
+++ src/api/question.js
@@ -5,27 +5,27 @@ export function GetQuestionnaire(surveyID,url,isDesign){
     data.surveyID = surveyID;
     data.url = url;
     data.isDesign = isDesign;
-    return get("/questionnaire",data);
+    return get(url,data);
 }
 //拿到交叉分析数据
 export function GetCrossData(QuestionID1,QuestionID2){
     let data = {};
     data.QuestionID1 = QuestionID1;
     data.QuestionID2 = QuestionID2;
-    return get('',data);
+    return get("/dataPre",data);
 }
 //拿到其他数据
 export function GetOtherData(surveyID){
     let data = {};
     data.surveyID = surveyID;
-    return get('',data);
+    return get("/dataPre",data);
 }
 //拿到暂存信息
 export function GetStoreFill(userName,surveyID){
     let data = {};
     data.userName = userName;
     data.surveyID = surveyID;
-    return get('',data)
+    return get('/questionnaireFill',data)
 }
 //传回填写信息
 export function PostFill(surveyID,status,question){
@@ -33,7 +33,7 @@ export function PostFill(surveyID,status,question){
     data.surveyID = surveyID;
     data.status = status;
     data.question = question;
-    return post('',data);
+    return post('/questionnaireFill',data);
 }
 //传回问卷信息
 export function PostQuestion(surveyID,title,category,isOrder,people,timeLimit,questionList){
@@ -45,5 +45,5 @@ export function PostQuestion(surveyID,title,category,isOrder,people,timeLimit,qu
     data.peoples = people;
     data.timeLimit = timeLimit;
     data.questionList = questionList;
-    return post('',data);
+    return post('/questionnaireDesign',data);
 }
